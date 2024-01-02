dae4042090dfdc76d3cdafc09d51097cadaf57d6
migoy99 <113483557+migoy99@users.noreply.github.com>
2024-01-02 10:28:34 +0800

    modified input sequence and added configure signing git commits script

--- a-configure-signing-git-commits.sh
+++ a-configure-signing-git-commits.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 echo ""
 echo "-------------------------------------------------------------"
-echo "   GitHub GPG Key Upload and Git Commit Signing Configuration"
+echo "               Git Commit Signing Configuration"
 echo "                        by: migoy99"
 echo "-------------------------------------------------------------"
 echo ""
 echo "Note: This script assumes that you already have GPG installed"
-echo "in your system or have generated a GPG key."
-echo "If not, Open a separate terminal and execute the following command" 
-echo "'gpg --full-generate-key' or visit:"
-echo "https://docs.github.com/en/authentication/managing-commit-signature-verification/generating-a-new-gpg-key"
-echo "for instructions."
+echo "in your system and have generated a GPG key and associated it"
+echo "to your GitHub Account."
 echo ""
 echo "Note: Ensure that the email associated with your GPG key"
 echo "matches your GitHub private email for signing Git commits."
 echo "This script requires your private GitHub email and username."
 echo ""
-echo "Note: Strongly suggested GPG key types are RSA or Ed25519."
-echo "Choose either of these when generating your GPG key."
-echo ""
-echo "Note: This script requires GitHub CLI (gh) for uploading your"
-echo "GPG key to GitHub. Ensure 'gh' is installed, and have a GitHub"
-echo "token ready, for authentication."
 echo "-------------------------------------------------------------"
 echo ""
 
@@ -33,38 +24,8 @@ echo "Look for the key ID usually found after 'pub rsa3072/', 'pub rsa4096/', 'p
 echo "Example: pub   rsa3072/E6C010C36F3C9CF0 where 'E6C010C36F3C9CF0' is the long key ID"
 echo ""
 
-# Input GPG key long key ID
+# Prompt for GPG key long key ID
 read -p "Enter your GPG key's long key ID: " GPG_LONG_KEY_ID
-
-# Store exported data in temporary text file
-gpg --export --armor $GPG_LONG_KEY_ID > temp_exported_key.txt
-
-
-# Prompt for GPG key details
-read -p "Enter your GPG key title: " GPG_KEY_TITLE
-
-# Prompt for GitHub token
-read -p "Enter your GitHub token: " GITHUB_TOKEN
-
-# Store token in a text file
-echo "$GITHUB_TOKEN" > token.txt
-
-# GitHub CLI auth login with token
-gh auth login --with-token < token.txt
-
-# Refresh authentication
-gh auth refresh -s write:gpg_key
-
-# Add GPG key to GitHub account using gh cli
-if gh gpg-key add temp_exported_key.txt; then
-    echo "GPG key added to GitHub account successfully."
-else
-    echo "Failed to add GPG key. Exiting..."
-    exit 1
-fi
-
-# Clean up: Remove the Exported GPG file
-rm temp_exported_key.txt
 
 # Prompt for private GitHub email associated with the GPG key
 read -p "Enter your private GitHub email associated with the GPG key: " GITHUB_EMAIL
@@ -118,4 +79,3 @@ echo "Git configuration completed successfully."
 echo "Check config by: git config --local --list"
 echo "Check config by: git config --global --list"
 echo ""
-
